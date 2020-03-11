ee87cfec09a3dff204278280716e2644e33410db
Romain Vo <romain.vo@hotmail.fr>
2020-03-11 18:28:03 +0100

    modification nb semaine

--- format.py
+++ format.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 #On met en forme le planning 
 planning = pd.read_csv('planning.txt', header='infer',
-                       index_col=['agent','jours','poste'])
+                       index_col=['agent','jour','poste'])
 planning = planning.unstack(level='poste')
 planning = planning.droplevel(level=0, axis=1)
 planning.loc[:, 'poste'] = planning.idxmax(axis=1).values
@@ -19,16 +19,16 @@ planning = planning.droplevel(level=0, axis=1)
 
 
 contraintes = pd.read_csv('contraintes.txt', header='infer',
-                          index_col=['agent','id_contrainte','jours'])
+                          index_col=['agent','contrainte','jour'])
 
 temp = contraintes.droplevel(level=[0,2], axis=0)
-count_contraintes_violees = temp.groupby(by='id_contrainte', axis=0).sum()
+count_contraintes_violees = temp.groupby(by='contrainte', axis=0).sum()
 
 temp = contraintes.droplevel(level=[1,2], axis=0)
 count_agents_violees = temp.groupby(by='agent', axis=0).sum()
 
 temp = contraintes.droplevel(level=2, axis=0)
-count_mix_viols = temp.groupby(by=['agent','id_contrainte'], axis=0).sum()
+count_mix_viols = temp.groupby(by=['agent','contrainte'], axis=0).sum()
 count_mix_viols = count_mix_viols.unstack(level='agent')
 count_mix_viols= count_mix_viols.droplevel(level=0, axis=1)
 count_mix_viols.loc[:, 'total'] = count_contraintes_violees.values
