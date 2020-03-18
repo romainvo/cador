3bea5f5d1eea09ca8a1f99394ce3166790371216
Romain Vo <romain.vo@hotmail.fr>
2020-03-18 22:11:30 +0100

    modification groupes de contrainte

--- format.py
+++ format.py
@@ -17,15 +17,27 @@ planning.drop(labels=[1,2,3,4,5,6,7], inplace=True, axis=1)
 planning = planning.unstack(level='agent')
 planning = planning.droplevel(level=0, axis=1)
 
+count_poste = pd.DataFrame(dtype='int64')
+for agent in planning.columns:
+    count_poste.loc[:, agent] = planning[agent].value_counts()
+count_poste.sort_index(inplace=True)
 
 contraintes = pd.read_csv('contraintes.txt', header='infer',
                           index_col=['agent','contrainte','jour'])
+contraintes_semaine = pd.read_csv('contraintes_semaine.txt', header='infer',
+                                  index_col=['agent','contrainte','semaine'])
 
 temp = contraintes.droplevel(level=[0,2], axis=0)
 count_contraintes_violees = temp.groupby(by='contrainte', axis=0).sum()
+temp =\
+contraintes_semaine.droplevel(level=[0,2], axis=0).groupby(by='contrainte', axis=0)
+count_contraintes_violees = count_contraintes_violees.append(temp.sum())
+count_contraintes_violees.sort_values(by='contrainte', inplace=True)
 
 temp = contraintes.droplevel(level=[1,2], axis=0)
 count_agents_violees = temp.groupby(by='agent', axis=0).sum()
+temp = contraintes_semaine.droplevel(level=[1,2], axis=0).groupby(by='agent', axis=0)
+count_agents_violees += temp.sum()
 
 temp = contraintes.droplevel(level=2, axis=0)
 count_mix_viols = temp.groupby(by=['agent','contrainte'], axis=0).sum()
